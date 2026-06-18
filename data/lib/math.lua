3dd2389aad9471db954a305ad455c4a1843ae9b5
Florian Rieder <florian.rieder@unil.ch>
2026-06-18 22:29:36 +0200

    Add lib.math with clamp, clamp01 and lerp

---
+++ data/lib/math.lua
@@ -0,0 +1,17 @@
+function clamp(value, min, max)
+  return math.max(min, math.min(max, value))
+end
+
+function clamp01(value)
+  return clamp(value, 0, 1)
+end
+
+function lerp(from, to, time)
+    return from + time * (to - from)
+end
+
+return { 
+  clamp = clamp,
+  clamp01 = clamp01,
+  lerp = lerp
+}
